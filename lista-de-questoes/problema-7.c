807427b4cfab38a32962c4dd7a7985f3e94382b0
Carlos Antunis <crlsantnys@gmail.com>
2025-04-08 20:05:38 -0300

    Minor fixes

--- lista-de-questoes/problema-7.c
+++ lista-de-questoes/problema-7.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 long long int inverso_modular(long long int, long long int);
+long long int euclides_estendido(long long int, long long int, long long int*, long long int*);
 
 int main() {
     long long int a, b;
@@ -23,14 +24,21 @@ int main() {
     return 0;                           // finaliza a execucao sem erros
 }
 
-long long int inverso_modular(long long int a, long long int b) {
-    const short True = 1;
-    int inv = 1;
-    while (True) {                      // enquanto o inverso modular nao for encontrado
-        if ((a * inv) % b == 1) {       // teste se inv eh o inverso modular
-            return inv;
-        }
-        //  teste o proximo
-        inv++;
+long long int euclides_extendido(long long int a, long long int b, long long int *x, long long int *y) {
+    if (b == 0) {
+        *x = 1;
+        *y = 0;
+        return a;
     }
+    long long int x1, y1;
+    long long int d = euclides_extendido(b, a % b, &x1, &y1);
+    *x = y1;
+    *y = x1 - (a / b) * y1;
+    return d;
+}
+
+long long int inverso_modular(long long int a, long long int b) {
+    long long int x, y;
+    long long int _ = euclides_estendido(a, b, &x, &y);
+    return x % b;
 }
